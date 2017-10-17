f0b456ecc175ca392d5b067b6d1f23094d16b6e4
Matthias <hanisch.matthias@googlemail.com>
2017-10-17 21:32:04 +0200

    Test for configuration texFile

---
+++ mathan-latex-maven-plugin-it/src/test/java/io/mathan/maven/latex/TexFileTest.java
@@ -0,0 +1,21 @@
+package io.mathan.maven.latex;
+
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.ResourceExtractor;
+import org.junit.Test;
+
+import java.io.File;
+
+public class TexFileTest {
+    @Test
+    public void pdf() throws Exception {
+        File dir = ResourceExtractor.simpleExtractResources(getClass(), "/tex_file");
+        Verifier verifier = new Verifier(dir.getAbsolutePath());
+        verifier.executeGoal("mathan:latex");
+        verifier.assertFilePresent("target/tex_file-0.0.2-SNAPSHOT.pdf");
+        // as there is no bibtex, makeindex file these steps will be skipped
+        verifier.verifyTextInLog("[mathan] execution skipped: bibtex");
+        verifier.verifyTextInLog("[mathan] execution skipped: makeindex");
+        verifier.verifyTextInLog("[mathan] execution skipped: makeindexnomencl");
+    }
+}
