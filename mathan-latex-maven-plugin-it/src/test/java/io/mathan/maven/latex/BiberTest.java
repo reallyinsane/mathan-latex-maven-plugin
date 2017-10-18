48c97c97357b78943883da8afb4b32d927edd231
Matthias <hanisch.matthias@googlemail.com>
2017-10-18 22:18:32 +0200

    Test for using bibtex/biber

--- mathan-latex-maven-plugin-it/src/test/java/io/mathan/maven/latex/BiberTest.java
+++ mathan-latex-maven-plugin-it/src/test/java/io/mathan/maven/latex/BiberTest.java
@@ -6,15 +6,15 @@ import org.junit.Test;
 
 import java.io.File;
 
-public class BibTest {
+public class BiberTest {
     @Test
     public void pdf() throws Exception {
-        File dir = ResourceExtractor.simpleExtractResources(getClass(), "/features/bib");
+        File dir = ResourceExtractor.simpleExtractResources(getClass(), "/features/biber");
         Verifier verifier = new Verifier(dir.getAbsolutePath());
         verifier.executeGoal("mathan:latex");
-        verifier.assertFilePresent("target/bib-0.0.2-SNAPSHOT.pdf");
+        verifier.assertFilePresent("target/biber-0.0.2-SNAPSHOT.pdf");
         // as there is no bibtex, makeindex file these steps will be skipped
-        verifier.verifyTextInLog("[mathan] execution: bibtex");
+        verifier.verifyTextInLog("[mathan] execution: biber");
         verifier.verifyTextInLog("[mathan] execution skipped: makeindex");
         verifier.verifyTextInLog("[mathan] execution skipped: makeindexnomencl");
     }
